008b125b061c0b281ee1dab852c6b6b6140ac2b2
AkinniyiS <157753154+AkinniyiS@users.noreply.github.com>
2024-10-17 09:15:38 -0400

    Created database tables

--- petapp/lib/database_helper.dart
+++ petapp/lib/database_helper.dart
@@ -27,4 +27,56 @@ class DatabaseHelper{
   static final columnTaskName = 'task_name';
   static final columnTaskTime = 'task_time';
   static final columnPetIdTaskFK = 'pet_id';
+
+  static Database? _database;
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  DatabaseHelper._privateConstructor();
+
+  Future<Database> get database async{
+    if(_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future <Database> _initDatabase() async{
+    String path = join (await getDatabasesPath(), _databaseName);
+    return await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  //Create Database tables
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE $petProfileTable(
+    $columnPetId INTEGER PRIMARY KEY AUTOINCREMENT,
+    $columnPetName TEXT NOT NULL, 
+    $columnSpecies TEXT NOT NULL,
+    $columnBreed TEXT, 
+    $columnAge INTEGER
+    )
+    ''');
+    
+    await db.execute('''
+    CREATE TABLE $healthRecordsTable(
+    $columnHealthId INTEGER PRIMARY KEY AUTOINCREMENT,
+    $columnVaccinationDate TEXT,
+    $columnRecordDetails TEXT,
+    $columnPetIdFK INTEGER,
+    FOREIGN KEY ($columnPetIdFK)REFERENCES $petProfileTable($columnPetId)
+    )
+    ''');
+
+    await db.execute('''
+      CREATE TABLE $dailyCareTable(
+      $columnTaskId INTEGER PRIMARY KEY AUTOINCREMENT,
+      $columnTaskName TEXT NOT NULL,
+      $columnTaskTime TEXT NOT NULL,
+      $columnPetIdTaskFK INTEGER,
+      FOREIGN KEY ($columnPetIdTaskFK) REFERENCES $petProfileTable ($columnPetId)
+      )
+    ''');
+  }
 }
